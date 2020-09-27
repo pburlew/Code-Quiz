1588e1d853e7cc363e00110f6c5cb7744f3daffd
Paige Burlew <paigeburlew@gmail.com>
2020-09-27 12:31:50 -0500

    completed and cleaned high scores js

--- Develop/assets/js/scores.js
+++ Develop/assets/js/scores.js
@@ -2,15 +2,30 @@ var clearBtnEl = document.querySelector("#clear");
 var highScoresEl = document.querySelector("#highscores");
 
 function printHighscores() {
-  // either get scores from localstorage or set to empty array
+  highScoresEl.innerHTML = "";
+  // get from localstorage
+  showHighScores = JSON.parse(localStorage.getItem("High Scores"));
+  
+  //check to see it works! 
+  console.log(showHighScores + "high scores are working");
+  
+
 
   // (optional) sort highscores by score property in descending order
 
   // for each score
+  for (var i =0; i < showHighScores.length; i++){
     // create li tag for each high score
+    var li = document.createElement("li");
+    li.textContent = (showHighScores[i].Initials + ": " + showHighScores[i].Score);
 
-    // display on page
-}
+     // display on page
+    highScoresEl.appendChild(li);
+  };
+  
+
+   
+};
 
 //use local storage to clear the saved high scores
 function clearHighscores() {
